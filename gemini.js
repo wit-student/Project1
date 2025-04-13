672f51fe9f7509078d15099f5b27bfd146381ee2
Sakshi Kapse <149087233+wit-student@users.noreply.github.com>
2025-04-13 15:52:43 +0530

    Update gemini.js

--- gemini.js
+++ gemini.js
@@ -42,7 +42,7 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(`${process.env.MONGO_URI}/${process.env.MONGO_DB_NAME}`);
+  await mongoose.connect(`${process.env.MONGO_URI}`);
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
